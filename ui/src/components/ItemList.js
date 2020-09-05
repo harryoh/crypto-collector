9f2490371423efefdadce5409045a7e204587cf3
Harry Oh <harry@5004.pe.kr>
2020-09-06 03:04:02 +0900

    Change UI

--- ui/src/components/ItemList.js
+++ ui/src/components/ItemList.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
 import Item from './Item';
-import Summary from './Summary';
 import axios from "axios";
-import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
+
 import TableContainer from '@material-ui/core/TableContainer';
 import Paper from '@material-ui/core/Paper';
-
-const styles = theme => ({
-  table: {
-    minWidth: 320
-  }
-});
 
 const parse_data = (data) => {
   return Object.keys(data)
@@ -34,6 +25,7 @@ class ItemList extends Component {
       try {
         const res = await axios.get(`${baseurl}/api/prices`);
         const data = parse_data(res.data);
+
         this.setState({
           response: res.data,
           data: data
@@ -49,30 +41,26 @@ class ItemList extends Component {
 
   render() {
     const { data, response } = this.state;
-    const { classes } = this.props;
-    const prices = data.map(
+
+    const items = data.map(
       ({Id, Name, Price, Timestamp}) => (
         <Item
           id={Id}
           key={Id}
           name={Name}
           price={Price}
+          data={response}
           timestamp={Timestamp}
         />
       )
     );
 
     return (
-      <TableContainer component={Paper}>
-        <Table className={classes.table} size="small" aria-label="a dense table">
-          <TableBody>
-            {prices}
-            <Summary data={response}/>
-          </TableBody>
-        </Table>
-      </TableContainer>
+        <TableContainer component={Paper}>
+          {items}
+        </TableContainer>
     );
   }
 }
 
-export default withStyles(styles)(ItemList);
+export default ItemList;
--- ui/src/components/ListTemplate.js
+++ ui/src/components/ListTemplate.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ListTemplate.css';
 
-const ListTemplate = ({children}) => {
-  return (
-    <main className="list-template">
-      <section className="list-wrapper">
-        { children }
-      </section>
-    </main>
-  );
+class ListTemplate extends Component {
+  state = {
+    now: ""
+  }
+
+  async componentDidMount() {
+    const clock = () => {
+      const tz_kr = 9*60*60;
+      const iso = new Date(new Date().getTime()+tz_kr*1000).toISOString();
+      this.setState({
+        now: iso.slice(-13, -5)
+      });
+      setTimeout(clock, 1000);
+    };
+
+    clock();
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <div>
+        <div className="clock">
+          { this.state.now }
+        </div>
+        <main className="list-template">
+          <section className="list-wrapper">
+            { children }
+          </section>
+        </main>
+      </div>
+    );
+  }
 };
 
 export default ListTemplate;
