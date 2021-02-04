bbbf28a9d09c63d8871d83a28f358679aefd2e54
Harry Oh <harry@5004.pe.kr>
2021-02-04 23:32:07 +0900

    Fix for cors

--- main.go
+++ main.go
@@ -340,10 +340,15 @@ func _cache() *cache2go.CacheTable {
 // corsMiddleware :
 func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-control-Allow-Methods", "GET")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, X-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 		c.Next()
 	}
 }
@@ -482,7 +487,7 @@ func main() {
 		go bithumbLastPrice(env, ch)
 		go bybitLastPrice(env, ch)
 		go currencyRate(env, ch)
-		go sendMonitorMessage(env)
+		// go sendMonitorMessage(env)
 
 		for {
 			select {
