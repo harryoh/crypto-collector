c7e3c5aa6002f1942edb0744f3c6c483fb6eb2a7
Harry Oh <harry@5004.pe.kr>
2021-05-10 01:06:36 +0900

    Change order for symbols

--- util/tools.go
+++ util/tools.go
@@ -8,10 +8,10 @@ func SymbolName(market *string) string {
 		res = "BTC"
 	case "ETH_KRW", "KRW-ETH", "ETHUSD":
 		res = "ETH"
-	case "XRP_KRW", "KRW-XRP", "XRPUSD":
-		res = "XRP"
 	case "EOS_KRW", "KRW-EOS", "EOSUSD":
 		res = "EOS"
+	case "XRP_KRW", "KRW-XRP", "XRPUSD":
+		res = "XRP"
 	// case "EOS_KRW", "KRW-EOS", "EOSUSD":
 	// 	res = "EOS"
 	case "USDKRW":
